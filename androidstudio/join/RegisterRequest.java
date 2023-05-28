510204b60c337870f4fd80c23ca4a5b88975c796
EunKyeong Cho <74223597+Zyayou@users.noreply.github.com>
2023-05-29 00:24:11 +0900

    Update RegisterRequest.java

--- androidstudio/join/RegisterRequest.java
+++ androidstudio/join/RegisterRequest.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 public class RegisterRequest extends StringRequest  {
     //서버 URL 설정(php 파일 연동)
-    final static private String URL = "http://ip/Register.php";
+    final static private String URL = "http://ip주소/Register.php";
     private Map<String, String> map;
     //private Map<String, String>parameters;
 
